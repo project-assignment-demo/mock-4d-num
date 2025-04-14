7f5762d54445a658e1a87ec2af6c967a8468975e
cleong <zhuzhuloh1998@gmail.com>
2025-04-14 17:49:20 +0800

    wip: update 4d hot number drop down styling

--- src/pages/Hot4DNumber/components/HotFourDTypeSelection/index.tsx
+++ src/pages/Hot4DNumber/components/HotFourDTypeSelection/index.tsx
@@ -18,10 +18,10 @@ const FourDTypesSelection = ({
               onUpdateSelectedFourD(item.id);
             }}
             key={item.id}
-            className="p-2 bg-white border-b"
+            className="p-2"
           >
-            <div className="flex flex-row items-center w-full">
-              <img className="w-[35px] h-[35px]" src={item.source} />
+            <div className="flex flex-row items-center gap-2  w-[256px]">
+              <img className="w-[35px] h-[35px] md:w-[40px] md:h-[40px]" src={item.source} />
               <p className="text-[14px]"> {item.label}</p>
               <div className="flex-1 basis-0 place-self-stretch"></div>
               <input type="radio" checked={item.id === selectedId} />
