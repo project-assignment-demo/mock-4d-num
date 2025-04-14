45ceafc60d0770dcf15298d70ce1d388a1163b95
cleong <zhuzhuloh1998@gmail.com>
2025-04-14 18:04:26 +0800

    wip: add missing styles

--- src/pages/Hot4DNumber/components/HotFourDTypeSelection/index.tsx
+++ src/pages/Hot4DNumber/components/HotFourDTypeSelection/index.tsx
@@ -21,10 +21,20 @@ const FourDTypesSelection = ({
             className="p-2 cursor-pointer"
           >
             <div className="flex flex-row items-center gap-2  w-[256px]">
-              <img className="w-[35px] h-[35px] md:w-[40px] md:h-[40px]" src={item.source} />
+              <img
+                className="w-[35px] h-[35px] md:w-[40px] md:h-[40px]"
+                src={item.source}
+              />
               <p className="text-[14px]"> {item.label}</p>
               <div className="flex-1 basis-0 place-self-stretch"></div>
-             <img className="w-[25px] h-[25px]" src={item.id === selectedId ? 'https://4dnum.com/assets/green-0b1baf2d.svg': 'https://4dnum.com/assets/gray-243f7312.svg'}/>
+              <img
+                className="w-[25px] h-[25px]"
+                src={
+                  item.id === selectedId
+                    ? "https://4dnum.com/assets/green-0b1baf2d.svg"
+                    : "https://4dnum.com/assets/gray-243f7312.svg"
+                }
+              />
             </div>
           </div>
         ))}
